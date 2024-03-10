5994bba30f2a9ae53e669602cabba69d5b27aba7
G.G.S <82466341+Zer0plusOne@users.noreply.github.com>
2024-03-10 19:09:59 +0100

    Update main.cpp
    + Codigo comentado completamente
    + Problemas con la generacion de los bodes del tablero solicucionados
    + Tengo hambre

--- Snake/Snake/main.cpp
+++ Snake/Snake/main.cpp
@@ -31,105 +31,140 @@ void gameSetup() {
 
 void gameStartDraw() {
 
-    system("cls"); // Limpia la consola
-    for (int i = 0; i < ancho + 2; i++)
-        cout << "-";
-    cout << endl;
+    system("cls"); // para dar la ilusion de la pantalla se actualiza en directo y no parezca una novela grafica
 
-    for (int i = 0; i < altura; i++) {
-        for (int j = 0; j < ancho; j++) {
-            if (j == 0)
-                cout << "|";
-            if (i == y && j == x)
-                cout << "X "; // Añade un espacio extra para la serpiente
-            else if (i == fruitY && j == fruitX)
-                cout << "O "; // Añade un espacio extra para la fruta
-            else {
-                bool print = false;
-                for (int k = 0; k < nTail; k++) {
-                    if (tailX[k] == j && tailY[k] == i) {
-                        cout << "x "; // Añade un espacio extra para la cola
-                        print = true;
+    for (int i = 0; i < ancho * 2 + 1 ; i++) { // idicamos que queremos rellenar de guiones la parte superior de manera que mientras i sea menor al doble + 1 del ancho (recordemos que hemos rellenado de *2* espacios el tablero)
+        cout << "-"; // rellena con guiones
+    }
+    cout << endl; // saltito de linia necesario para que no se junte con el tablero para luego no molestar a la hora de rellenar los bordes
+
+    for (int i = 0; i < altura; i++) { // bucle para toquetear cada fila del tablero
+        for (int a = 0; a < ancho; a++) { // siempre que  "a" (no puedo usar i porque ya esta usada y estoy dentro de su bucle) sea menor al ancho...
+            if (a == 0) { // cuando a sea igual a 0
+                cout << "|"; // el valor de indice 0 de cada linia sera rellenado con el caracter "pipe" GRACIAS RAFALAGOON POR TUS ENSEÑANZAS
+            }
+            if (i == y && a == x) { // si i y a son iguales a la posicion de la cabeza de la serpiente...
+                cout << "X "; // imprimo el caracter "X" y añado un espacio extra para la serpiente
+            }
+            else if (i == fruitY && a == fruitX) { // si i y a son iguales a las coordenadas de la posicion de la fruta...
+                cout << "O "; // dubujamos la frutita con un espacio igual porque sino sale torcida si te mueves cerca de ella
+            }
+            else { // siempre que  "a" (no puedo usar i porque ya esta usada y estoy dentro de su bucle) NO sea menor al ancho...
+                bool cola = false; // declaramos una variable que se encargara de comprovar si la cola esta donde debe de estar
+                for (int b = 0; b < nTail; b++) { // para toquetear el tablero pero en este caso para ver donde se pone la cola de la serpiente
+                    if (tailX[b] == a && tailY[b] == i) { // si la coodenada de la cola en x y y es igual...
+                        cout << "x "; // imprimimos el valor de la cola (una X pero minuscula)
+                        cola = true; // terminamos de imprimir por ahora ya que no hay nada que nos indique que necesitamos imprimir mas colas
                     }
                 }
-                if (!print)
-                    cout << "  "; // Añade dos espacios para las celdas vacías
+                if (!cola) { // en caso de que la cola este en false...
+                    cout << "  "; // añado dos espacios al final
+                }
             }
-            if (j == ancho - 1)
-                cout << "|";
+            if (a == ancho - 1) { // imprimimos el borde derecho
+                cout << "|"; // caracter del bode derecho
+            }
         }
-        cout << endl;
+        cout << endl;// terminamos linia porque sino imprimiria la siguiente al lado y no abajo
     }
 
-    for (int i = 0; i < ancho + 2; i++)
-        cout << "-";
-    cout << endl;
+    for (int i = 0; i < ancho * 2 + 1; i++) { // lo mismo de arriba pero para el borde inferior
+        cout << "-"; // caracter para el borde inferior...
+    }
+    cout << endl; // damos un espacio de linia despues del bucle for ya que sino la puntuacion la junta con la linia inferior (motivos esteticos)
 
-    cout << "Score: " << punt << endl;
+    cout << "Puntuacion: " << punt << endl; // puntuacion que imprime tambien el valor hasta el momento de la variable "punt" 
 }
 
-void Input() {
-    if (IsAPressed()) dir = LEFT;
+void userInput() { // funcion para los imputs del usuario usando ' keyboard.h '
+    if (IsAPressed()) dir = LEFT; // no hace falta que comente que hace, simplemente le damos a WASD los valores "estandard" de movimiento
     else if (IsDPressed()) dir = RIGHT;
     else if (IsWPressed()) dir = UP;
     else if (IsSPressed()) dir = DOWN;
 }
 
-// Añadir aquí las variables globales y la inicialización del juego (ver ejemplos anteriores)
-void Logic() {
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = x;
-    tailY[0] = y;
-    for (int i = 1; i < nTail; i++) {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
+// logica del juego ( *IMPORTANTE*  RECUERDA QUE ESTO ESTARA DENTRO DE UN BUCLE WHILE, ASI QUE NO HACE FALTA TRABAJAR DENTRO DE UN BUCLE)
+void gameLogic() {
+
+    /* serpiente inicial */
+
+    int prevX = tailX[0]; // variable para inicializar la posicion de la cola de la serpiente (la serpiente) en el eje X
+    int prevY = tailY[0]; // variable para inicializar la posicion de la cola de la serpiente (la serpiente) en el eje y
+
+    /* serpiente in game */
+
+    int prev2X, prev2Y; // inicializamos las variables que serviran para almacenar las coordenadas previas de los segmentos de la cola mientras esta se mueve
+    tailX[0] = x; // para que coincida con la posicion actual del movimiento con la impresion de la serpiente en el eje x
+    tailY[0] = y; // para que coincida con la posicion actual del movimiento con la impresion de la serpiente en el eje y
+
+    for (int i = 1; i < nTail; i++) { // iteramos cada segmento de la serpiente (menos el de incide 0 porque es la cabeza)
+        prev2X = tailX[i]; // actualizamos segun la direccion en la que se encuentre en ese frame en el eje X
+        prev2Y = tailY[i]; // actualizamos segun la direccion en la que se encuentre en ese frame en el eje Y
+        tailX[i] = prevX; // justo despues realizamos lo mismo pero para toda la cola en el eje X
+        tailY[i] = prevY; // justo despues realizamos lo mismo pero para toda la cola en el eje X
         prevX = prev2X;
         prevY = prev2Y;
     }
-    switch (dir) {
+    switch (dir) { // iniciamos un switch (es lo mejor que se me ha ocurrido) para las direcciones, indicando que pasa cuando la variable "dir" esta en alguno de los casos de abajo
     case LEFT:
-        x--;
+        x--; // en caso de ir a la derecha se movera en negativo en relacion al tablero (ancho)
         break;
     case RIGHT:
-        x++;
+        x++; // en caso de ir a la izquierda se movera en positivo en relacion al tablero (ancho)
         break;
     case UP:
-        y--;
+        y--; // en caso de ir arriba se movera en negativo en relacion al tablero (alto)
         break;
     case DOWN:
-        y++;
-        break;
-    default:
+        y++; // en caso de ir abajo se movera en positivo en relacion al tablero (alto)
         break;
     }
-    if (x >= ancho) x = 0; else if (x < 0) x = ancho - 1;
-    if (y >= altura) y = 0; else if (y < 0) y = altura - 1;
 
-    for (int i = 0; i < nTail; i++)
-        if (tailX[i] == x && tailY[i] == y)
-            gameOver = true;
+    /* ESTE BLOQUE ESTABA HECHO PARA DEBUG, ELIMINARLO O COMENTARLO DEBERIA SUPONER QUE LA SERPIENTE MUERA AL TOCAR LOS BORDES DE LA PANTALLA */
 
-    if (x == fruitX && y == fruitY) {
-        punt += 10;
-        fruitX = rand() % ancho;
-        fruitY = rand() % altura;
-        nTail++;
+    // BLOQUE DE DEBUG: este bloque (porque me has dicho que lo deje Alex) es lo que hace que la serpiente en caso de entrar en el borde de la pantalla salga por el lado opuesto, a continuacion comento lo que hace cada cosa.
+    
+    if (x >= ancho) { // en caso de que la coordenada X sea mayor o igual al ancho del tablero...
+        x = 0; // cambiamos la coordenada X por un 0
+    }
+    else if (x < 0) { // en caso de que la coordenada x sea menor a 0 (indica que ha intentado salir por el lado izquierdo)...
+        x = ancho - 1; // la coordenada cambia por el ancho total del tablero -1 (ya que el total indica que es el borde)
+    }
+    if (y >= altura) { // en caso de que la coordenada X sea mayor o igual a la altura del tablero...
+        y = 0; // cambiamos la coordenada Y por un 0
+    }
+    else if (y < 0) { // en caso de que la coordenada Y sea menor a 0 (indica que ha intentado salir por arriba)...
+        y = altura - 1; // // la coordenada cambia por la altura total del tablero -1 (ya que el total indica que es el borde)
+    }
+
+    // este bucle comprueba por cada frame que las coordenadas de la cabeza y de la cola no coincidan en ningun punto, en caso de hacerlo...
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y) { // en caso de que las coordenadas de la cabeza coincidan con alguna de las posiciones en la cola...
+            gameOver = true; // cambiamos el valor a true del boolean que indica el GAME OVER
+        }
+    }
+    // este bucle for se indica a comparar las coordenadas de la cabeza y la fruta en el tablerom en caso de coincidir...
+    if (x == fruitX && y == fruitY) { // si las coordenadas en X y Y coinciden entre la fruta y la cabeza de la serpiente...
+        punt += 10; // se le suman 10 puntos a la variable que almacena la puntuacion
+        fruitX = rand() % ancho; // se generan nuevas coordenadas en el eje X para la fruta
+        fruitY = rand() % altura; // se generan nuevas coordenadas en el eje Y para la fruta
+        nTail++; // se suma 1 caracter mas a la cola de la serpiente
     }
 }
 
 int main() {
-    gameSetup(); // Inicializar el juego
 
-    bool bGameOver = false;
+    gameSetup(); // inicializamos el juego ( todas las variables necesarias para su funcionamiento)
 
-    while (!bGameOver) {
-        gameStartDraw(); // Dibujar el tablero y la serpiente
-        Input(); // Manejar la entrada del usuario
-        Logic(); // Actualizar la lógica del juego
+    bool bGameOver = false; // declaramos el estado de false al boolean que funciona para el BUCLLE no para la partida
+
+    while (!bGameOver) { // mientras ese boolean no se modifique...
+
+        gameStartDraw(); // dibujamos el tablero, la serpiente y todo lo que se cante
+
+        userInput(); // manejamos el input del usuario para que por cada iteracion de este bucle se actualice posteriormente }
+                                                                                                                          /*EL ORDEN DE ESTOS TIENE QUE SER ASI PORQUE SINO NO FUNCIONA*/
+        gameLogic(); // actualizamos la logica del juego para que con el siguente frame se actualice todo de manera correcta }
 
         //Sleep main thread to control game speed execution
         std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_RATE));
